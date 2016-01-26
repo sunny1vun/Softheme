1f8f7a423cd9d3fbfc6276f66f5ee449615b2eb0
sunny1vun <sunny1vun@users.noreply.github.com>
2016-01-26 21:39:14 +0200

    Create YEPGroup.m

---
+++ YEPGroup.m
@@ -0,0 +1,14 @@
+//
+//  YEPGroup.m
+//  HomeTask2
+//
+//  Created by 2 on 1/26/16.
+//  Copyright В© 2016 123. All rights reserved.
+//
+
+#import "YEPGroup.h"
+
+@implementation YEPGroup
+
+
+@end
