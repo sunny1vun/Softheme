c96f1532d746fb4de89ae04eab726cc21df822fd
sunny1vun <sunny1vun@users.noreply.github.com>
2016-01-26 17:53:47 +0200

    Create YONSArray+Utility.h

---
+++ YONSArray+Utility.h
@@ -0,0 +1,17 @@
+//
+//  YONSArray+Utility.h
+//  HomeTask2
+//
+//  Created by 2 on 1/26/16.
+//  Copyright © 2016 123. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "YOGroup.h"
+
+@interface YONSArray(Utility)
+
+@property YOGroup* group;
+-(void)PrettyViewOfAllElements:(int)index;
+
+@end
