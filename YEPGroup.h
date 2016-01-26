f4bb997e0eedc18bd4746ae32efe18efdf2a282d
sunny1vun <sunny1vun@users.noreply.github.com>
2016-01-26 21:38:50 +0200

    Create YEPGroup.h

---
+++ YEPGroup.h
@@ -0,0 +1,18 @@
+//
+//  YEPGroup.h
+//  HomeTask2
+//
+//  Created by 2 on 1/26/16.
+//  Copyright © 2016 123. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "YEPArtist.h"
+
+@interface YEPGroup : YEPArtist
+
+@property NSArray *listOfArtists;
+
+//создать метод возвращающий чето там
+
+@end
