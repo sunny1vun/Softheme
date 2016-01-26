8f111380d4abe79bb7d7bdf287d893c25c5b7673
sunny1vun <sunny1vun@users.noreply.github.com>
2016-01-26 17:51:50 +0200

    Create group.h

---
+++ group.h
@@ -0,0 +1,17 @@
+//
+//  Group.h
+//  HomeTask2
+//
+//  Created by 2 on 1/26/16.
+//  Copyright © 2016 123. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Artist.h"
+
+@interface Group : Artist
+
+@property NSArray *listOfArtists;
+
+
+@end
