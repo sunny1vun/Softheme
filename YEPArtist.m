cb4f230c772cc1bd27452a76d0ac4e5954a459ff
sunny1vun <sunny1vun@users.noreply.github.com>
2016-01-27 02:47:44 +0200

    Update YEPArtist.m

--- YEPArtist.m
+++ YEPArtist.m
@@ -1,5 +1,5 @@
 //
-//  YEPArtist.h
+//  YEPArtist.m
 //  HomeTask2
 //
 //  Created by 2 on 1/26/16.
@@ -7,16 +7,39 @@
 //
 
 #import <Foundation/Foundation.h>
-
+#import "YEPArtist.h"
+#import "YEPSong.h"
 //this line provides to inherit class NSObject
-@interface YEPArtist : NSObject
-
-@property NSString *name;
-@property NSArray *listOfSongs;
+@implementation YEPArtist : NSObject
 
 -(instancetype) initWithName:(NSString*)name
-                 listOfSongs:(NSArray*)listOfSongs;
+                 listOfSongs:(NSArray*)listOfSongs{
+    self= [super init];
+    if(!self) return nil;
+    
+    _name= name;
+    _listOfSongs= listOfSongs;
+    
+    return self;
+}
 
--(NSString*) descriprtion;
+-(NSString*) description{
+    NSMutableString *string = [[NSMutableString alloc] init];
+    
+    YEPArtist *artist= [super init];
+    [string appendFormat:@"\n%@ - Artist \n", artist.name];
+    [string appendString:@"List of songs ("];
+    int i=0;
+    for(YEPSong* song in artist.listOfSongs){
+        [string appendFormat:@"\"%@\"", song.name];
+        if(i<artist.listOfSongs.count-1){
+            [string appendString:@", "];
+            i++;
+        }
+    }
+    [string appendString:@")\n"];
+    
+    return string;
+}
 
 @end
